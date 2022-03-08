897affb0adcf277bb992715e76fcdae4b6a201e5
jaeson-dev <jaeson.dev@gmail.com>
2022-03-08 14:38:14 +0900

    Initial Commit

---
+++ lazyVGrid_tutorial_self/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lazyVGrid_tutorial_self
+//
+//  Created by Jaeson.dev on 2022/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lazyVGrid_tutorial_self/lazyVGrid_tutorial_selfApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyVGrid_tutorial_selfApp.swift
+//  lazyVGrid_tutorial_self
+//
+//  Created by Jaeson.dev on 2022/03/08.
+//
+
+import SwiftUI
+
+@main
+struct lazyVGrid_tutorial_selfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
